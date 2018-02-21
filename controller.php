1bd441150cc83cc44893f38c654492eae8e60532
zackaria59 <zachour59@gmail.com>
2018-02-21 09:04:21 +0100

    modif controller destination

--- controller.php
+++ controller.php
@@ -2,24 +2,24 @@
 
 if(isset($_GET["action"]) && isset($_POST["id"]) && isset($_POST["mdp"]))
 {
-	
+
 		if($_GET["action"]=="connexion")
 		{
 			require("traitementConnexion.php");
-			
+
 			if(connexion_autorise($_POST ["id"],$_POST ["mdp"]))
 			{
 				if($_SESSION['type']==1)
-				header("Location:/projets6/admin/appAdmin.php");
+				header("Location:/L3-S6-DCA/admin/appAdmin.php");
 				else if($_SESSION['type']==2)
-				header("Location:/projets6/admin/appProfesseur.php");
+				header("Location:/L3-S6-DCA/admin/appProfesseur.php");
 				else if($_SESSION['type']==3)
-				header("Location:/projets6/admin/appEtudiant.php");
+				header("Location:/L3-S6-DCA/admin/appEtudiant.php");
 			}
 			else{
-				header("Location:/projets6");
+				header("Location:/L3-S6-DCA");
 			}
-				
+
 		}
 }
 
